7f55eb0daee74d85aead885848fc36a072047943
huzzyz <69323208+huzzyz@users.noreply.github.com>
2021-10-27 17:50:38 +0400

    Update base.sh

--- base.sh
+++ base.sh
@@ -11,13 +11,13 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 w540.localdomain arch" >> /etc/hosts
 echo "192.168.1.53 Solitude" >> /etc/hosts  
 echo root:password | chpasswd
-pacman -S grub efibootmgr networkmanager network-manager-applet wget dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset flatpak nss-mdns acpid os-prober ntfs-3g terminus-font
+#pacman -S grub efibootmgr networkmanager network-manager-applet wget dialog wpa_supplicant mtools dosfstools base-devel linux-headers avahi xdg-user-dirs xdg-utils nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset flatpak nss-mdns acpid os-prober ntfs-3g terminus-font
 #pacman -S --noconfirm xf86-video-amdgpu
-pacman -S --noconfirm xf86-video-intel
-pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+#pacman -S --noconfirm xf86-video-intel
+#pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
 
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+#grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+#grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 systemctl enable cronie
